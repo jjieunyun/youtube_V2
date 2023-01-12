5626e2426b75989d466d2b6a5b6e661a67760f2d
jieun <lesson0186@gmail.com>
2023-01-12 23:59:19 +0900

    feat:Videos 페이지 구현 & Fake Api 인스턴스 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet } from "react-router-dom";
+import SearchHeader from "./components/SearchHeader";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <Outlet />
+      <SearchHeader />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,5 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useParams } from "react-router-dom";
+import FakeYoutube from "../api/fakeYoutube";
+import { search } from "../api/youtube";
+import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
-  return <div>Videos</div>;
+  const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], () => {
+    const youtube = new FakeYoutube();
+    return youtube.search(keyword);
+  });
+
+  return (
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : "❤️‍🔥"}</div>
+      {isLoading && <p>Lodaing...</p>}
+      {error && <p>Something is Wrong</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
