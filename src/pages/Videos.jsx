8564abee2e7220e497445688806f7a9b1f5a997f
jieun <lesson0186@gmail.com>
2023-01-13 00:02:22 +0900

    Revert "feat:Videos 페이지 구현 & Fake Api 인스턴스 만들기"
    This reverts commit 5626e2426b75989d466d2b6a5b6e661a67760f2d.

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,33 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import { useParams } from "react-router-dom";
-import FakeYoutube from "../api/fakeYoutube";
-import { search } from "../api/youtube";
-import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
-  const { keyword } = useParams();
-  const {
-    isLoading,
-    error,
-    data: videos,
-  } = useQuery(["videos", keyword], () => {
-    const youtube = new FakeYoutube();
-    return youtube.search(keyword);
-  });
-
-  return (
-    <>
-      <div>Videos {keyword ? `🔍${keyword}` : "❤️‍🔥"}</div>
-      {isLoading && <p>Lodaing...</p>}
-      {error && <p>Something is Wrong</p>}
-      {videos && (
-        <ul>
-          {videos.map((video) => (
-            <VideoCard key={video.id} video={video} />
-          ))}
-        </ul>
-      )}
-    </>
-  );
+  return <div>Videos</div>;
 }
