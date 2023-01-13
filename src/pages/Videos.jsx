f46db3b1aede4e0c5c1c7f6b6118d120791dfb91
jieun <lesson0186@gmail.com>
2023-01-13 23:54:16 +0900

    feat:.env파일 생성 & api설정(DI)

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,20 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
-import FakeYoutube from "../api/fakeYoutube";
-import { search } from "../api/youtube";
 import VideoCard from "../components/VideoCard";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => {
-    const youtube = new FakeYoutube();
-    return youtube.search(keyword);
-  });
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
 
   return (
     <>
